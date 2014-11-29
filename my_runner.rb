21eec82479fea4c48a6d028150082f1253ff33ce
Greg <gouwa5700@gmail.com>
2014-11-29 10:58:11 +0800

    fix: you can't add element to an array while you're in it's each block, that will run forever

--- my_runner.rb
+++ my_runner.rb
@@ -3,7 +3,8 @@ require 'pry'
 
 module Runner
   URL_PREFIX = "http://d.360buy.com/area/get?fid="
-  NODE_CONTAINER = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,42,43,84]
+  ROOT_NODE_CONTAINER = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,42,43,84]
+  LEAF_NODE_CONTAINER = []
   @@current_depth = 1
   ALLOWED_DEPTH = 3
   AGENT = Mechanize.new
@@ -39,7 +40,7 @@ module Runner
     if parse_result.class == Array and not parse_result.empty?
       puts "Accepted"
       children_array = parse_result
-      NODE_CONTAINER.concat(children_array)
+      LEAF_NODE_CONTAINER.concat(children_array)
       children_array.each do |child|
         child["parent_id"] = parent_id
         find_and_save_all_children(child["id"])
@@ -51,11 +52,11 @@ module Runner
 
   def self.run
     puts "---------START-----------"
-    NODE_CONTAINER.each do |root_node|
+    ROOT_NODE_CONTAINER.each do |root_node|
       find_and_save_all_children(root_node)
     end
     File.open("./jd_areas.json", "w") do |file|
-      file.write JSON.generate(NODE_CONTAINER)
+      file.write JSON.generate(LEAF_NODE_CONTAINER)
     end
     puts "----------END-----------"
   end
