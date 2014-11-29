ae16f41a0c465fd8ac760c7026de0541cdcab18f
Greg <gouwa5700@gmail.com>
2014-11-29 11:21:11 +0800

    fix: correct depth algorithm

--- my_runner.rb
+++ my_runner.rb
@@ -5,7 +5,6 @@ module Runner
   URL_PREFIX = "http://d.360buy.com/area/get?fid="
   ROOT_NODE_CONTAINER = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,42,43,84]
   LEAF_NODE_CONTAINER = []
-  @@current_depth = 1
   ALLOWED_DEPTH = 3
   AGENT = Mechanize.new
 
@@ -34,18 +33,18 @@ module Runner
     end
   end
 
-  def self.find_and_save_all_children(parent_id)
-    if (@@current_depth > 0) and (@@current_depth <= ALLOWED_DEPTH)
-      @@current_depth += 1
+  def self.find_and_save_all_children(parent_id, parent_depth)
+    if (parent_depth >= 0) and (parent_depth <= ALLOWED_DEPTH)
       url = "#{URL_PREFIX}#{parent_id}"
       parse_result = get_page_then_parse_until_succeed(url)
       if parse_result.class == Array and not parse_result.empty?
         puts "Accepted"
         children_array = parse_result
+        children_depth = parent_depth + 1
         LEAF_NODE_CONTAINER.concat(children_array)
         children_array.each do |child|
           child["parent_id"] = parent_id
-          find_and_save_all_children(child["id"])
+          find_and_save_all_children(child["id"], children_depth)
         end
       else
         puts "Rejected"
@@ -56,7 +55,7 @@ module Runner
   def self.run
     puts "---------START-----------"
     ROOT_NODE_CONTAINER.each do |root_node|
-      find_and_save_all_children(root_node)
+      find_and_save_all_children(root_node, 0)
     end
     File.open("./jd_areas.json", "w") do |file|
       file.write JSON.generate(LEAF_NODE_CONTAINER)
