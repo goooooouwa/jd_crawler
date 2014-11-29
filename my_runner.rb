66dfbd12c9df8610303ca32012e503987c772e0f
Greg <gouwa5700@gmail.com>
2014-11-29 11:53:35 +0800

    move root nodes to file so that we can replace the roots. This allows recurse crawling.

--- my_runner.rb
+++ my_runner.rb
@@ -3,7 +3,6 @@ require 'pry'
 
 module Runner
   URL_PREFIX = "http://d.360buy.com/area/get?fid="
-  ROOT_NODE_CONTAINER = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,42,43,84]
   NODE_CONTAINER = []
   ALLOWED_DEPTH = 1
   AGENT = Mechanize.new
