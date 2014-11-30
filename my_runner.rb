e7b13a5904dd1ba796907eb55ee13728fa893a8f
Greg <gouwa5700@gmail.com>
2014-11-30 08:30:33 +0800

    big refactor: depth => height. move height checking into next iteration. correct logic for dealing with childless parent

--- my_runner.rb
+++ my_runner.rb
@@ -5,7 +5,7 @@ module Runner
   URL_PREFIX = "http://d.360buy.com/area/get?fid="
   NODE_CONTAINER = []
   NODE_BLACK_LIST = []
-  ALLOWED_DEPTH = 1
+  MAX_HEIGHT = 1
   AGENT = Mechanize.new
 
   def self.get_page_until_succeed(url)
@@ -33,44 +33,53 @@ module Runner
     end
   end
 
-  def self.find_and_save_all_children(parent, parent_depth)
+  def self.find_and_save_all_children(parent, parent_height)
     if NODE_BLACK_LIST.include?(parent)
       binding.pry
       return
     end
 
-    if (parent_depth >= 0) and (parent_depth <= ALLOWED_DEPTH)
-      url = "#{URL_PREFIX}#{parent['id']}"
-      parse_result = get_page_then_parse_until_succeed(url)
-      if parse_result.class == Array and not parse_result.empty?
-        puts "Child Accepted"
-        children_array = parse_result
-        children_depth = parent_depth + 1
-        NODE_CONTAINER.concat(children_array)
-        children_array.each do |child|
-          child["parent_id"] = parent["id"]
-          find_and_save_all_children(child, children_depth)
-        end
-      elsif parse_result.class == Hash
-        puts "Child Rejected and also reject all sublings consequently to reduce time."
-        NODE_BLACK_LIST.push(parse_result)
-        binding.pry
-      elsif parse_result.class == Array and parse_result.empty?
-        puts "Child Rejected and also reject all sublings consequently to reduce time."
-        NODE_BLACK_LIST.concat(parse_result)
-        binding.pry
-      else
-        puts "oops"
-        binding.pry
+    parse_result = get_page_then_parse_until_succeed("#{URL_PREFIX}#{parent['id']}")
+    if parse_result.class == Array and not parse_result.empty?
+      # 1. got all children of parent
+      puts "Got all children of parent"
+      children_array = parse_result
+      children_height = parent_height + 1
+      children_array.each do |child|
+        child["parent_id"] = parent["id"]
       end
+      NODE_CONTAINER.concat(children_array)
+
+      if (children_height >= 0) and (children_height < MAX_HEIGHT)
+        # if allowed to go deeper
+        children_array.each do |child|
+          find_and_save_all_children(child, children_height)
+        end
+      end
+    elsif (parse_result.class == Hash) or (parse_result.class == Array and parse_result.empty?)
+      # 2. got one child or an empty array which indicates the parent has no child.
+      # NOTICE we assume if a node has no child, then the node's siblings have no child too.
+      # This is to optimize the traversing since the majority of time is spent on checking bad nodes.
+      puts "Parent has no child. Assume so are the parent's sublings."
+      unless parent["parent_id"].nil?
+        # TODO: how to get parent siblings
+        parent_siblings = nodes whose parent_id equals parent["parent_id"]
+        NODE_BLACK_LIST.concat(parent_siblings)
+      end
+    else
+      # 4. unknown
+      puts "oops"
+      binding.pry
     end
   end
 
   def self.run
     puts "---------START-----------"
     root_nodes = JSON.parse(File.open("./root_nodes.json").read())
-    root_nodes.each do |root_node|
-      find_and_save_all_children(root_node, 0)
+    if MAX_HEIGHT > 0
+      root_nodes.each do |root_node|
+        find_and_save_all_children(root_node, 0)
+      end
     end
     File.open("./jd_areas.json", "w") do |file|
       file.write JSON.generate(NODE_CONTAINER)
